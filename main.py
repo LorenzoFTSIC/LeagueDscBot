260f3a2e442d97a975fb78e570426ab9bf9f303f
Lorenzo <lorenzoferritsic@gmail.com>
2024-12-31 13:09:31 -0500

    Added command to request PUUID

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 from dotenv import load_dotenv
 import requests
 
+
 load_dotenv()
 discordToken = os.getenv("DISCORDTOKEN")
 riotToken = os.getenv("RIOTTOKEN")
@@ -14,8 +15,9 @@ def getPUUID():
     tag = "fart"
     url = "https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/" + gameName + "/" + tag + "?api_key=" + riotToken
     response = requests.get(url)
-    print(response.json())  
-    return response.json()
+    returnedJSON = response.json()
+    playerPUUID = returnedJSON.get("puuid", None)
+    return playerPUUID
 
 def getMatch():
     pass
@@ -23,15 +25,27 @@ def getMatch():
 @client.event 
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
+    playerPUUID = getPUUID()
+    if playerPUUID:
+        print("PUUID acquired: " + playerPUUID)
+    else:
+        print("Failed to fetch PUUID")
+
 
 @client.event 
 async def on_message(message):
+    playerPUUID = getPUUID()
     if message.author == client.user:
         return
     if message.content.startswith("$hello"):
         print("replying Hello")
         await message.channel.send("Hello!")
-        await message.channel.send(getPUUID())
-    # await message.channel.send(message)
+    if message.content.startswith("$puuid"):
+        if playerPUUID:
+            await message.channel.send("Ashkon's PUUID is: " + playerPUUID)
+        else:
+            print("Failed to fetch PUUID")
+            await message.channel.send("Failed to fetch PUUID")
+
 
 client.run(discordToken)
